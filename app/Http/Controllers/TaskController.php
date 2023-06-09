ce8cf93e09282266bc20f7f41d3b3fc1bee0b1ea
decaffinetedturtle <syakirahmadakhtar@gmail.com>
2023-06-09 10:09:01 +0800

    first commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $tasks = Auth::user()->tasks;
+        //return view('dashboard', compact('tasks'));
+        return view('dashboard', ['tasks' => $tasks]);
+    }
+
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('tasks.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|max:255',
+            'description' => 'required',
+            'priority' => 'required|in:low,medium,high',
+        ]);
+
+        $task = new Task([
+            'title' => $request->get('title'),
+            'description' => $request->get('description'),
+            'priority' => $request->get('priority'),
+        ]);
+        $task->user_id = Auth::id();
+        $task->save();
+
+        return redirect('/dashboard')->with('success', 'Task saved!');
+    }
+
+
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,69 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <form method="post" action="{{ route('tasks.store') }}" class="space-y-4">
+                        @csrf
+                        <div>
+                            <label for="title" class="block text-sm font-medium text-gray-700">Title:</label>
+                            <input type="text"
+                                class="mt-1 block w-full rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                                name="title" style="color: black;" />
+                        </div>
+                        <div>
+                            <label for="description"
+                                class="block text-sm font-medium text-gray-700">Description:</label>
+                            <textarea class="form-control mt-1 block w-full rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                                name="description" style="color: black;"></textarea>
+                        </div>
+                        <div>
+                            <label for="priority" class="block text-sm font-medium text-gray-700">Priority:</label>
+                            <select
+                                class="form-control mt-1 block w-full rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                                style="color: black;" name="priority">
+                                <option value="low">Low</option>
+                                <option value="medium">Medium</option>
+                                <option value="high">High</option>
+                            </select>
+                        </div>
+                        <button type="submit"
+                            class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Add
+                            task</button>
+                    </form>
+
+                    <div class="mt-4">
+                        @if ($tasks)
+                            @foreach ($tasks as $task)
+                                @php
+                                    $color = 'white';
+                                    if ($task->priority === 'low') {
+                                        $color = 'bg-green-100';
+                                    }
+                                    if ($task->priority === 'medium') {
+                                        $color = 'bg-yellow-100';
+                                    }
+                                    if ($task->priority === 'high') {
+                                        $color = 'bg-red-100';
+                                    }
+                                @endphp
+                                <div class="p-6 rounded-md mt-4 {{ $color }}">
+                                    <h2 class="font-bold text-gray-700">{{ $task->title }}</h2>
+                                    <p class="text-gray-700">{{ $task->description }}</p>
+                                </div>
+                            @endforeach
+                        @else
+                            <p>No tasks found.</p>
+                        @endif
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
