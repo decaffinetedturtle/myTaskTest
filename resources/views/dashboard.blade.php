33e443f0bcba498c32af661d2e14852afed21579
decaffinetedturtle <syakirahmadakhtar@gmail.com>
2023-06-09 17:03:53 +0800

    Made update work

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -66,11 +66,39 @@
                                     <form method="post" action="{{ route('tasks.update', $task) }}" class="mt-2">
                                         @csrf
                                         @method('PUT')
-                                        <!-- Include inputs for title, description, and priority here... -->
+                                        <div class="my-2">
+                                            <label for="title"
+                                                class="block text-sm font-medium text-gray-900">Title:</label>
+                                            <input type="text" name="title" value="{{ $task->title }}"
+                                                class="mt-1 block w-full rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                                                style="color: black;" />
+                                        </div>
+                                        <div class="my-2">
+                                            <label for="description"
+                                                class="block text-sm font-medium text-gray-900">Description:</label>
+                                            <textarea name="description"
+                                                class="form-control mt-1 block w-full rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                                                style="color: black;">{{ $task->description }}</textarea>
+                                        </div>
+                                        <div class="my-2">
+                                            <label for="priority"
+                                                class="block text-sm font-medium text-gray-900">Priority:</label>
+                                            <select name="priority"
+                                                class="form-control mt-1 block w-full rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                                                style="color: black;">
+                                                <option value="low" {{ $task->priority == 'low' ? 'selected' : '' }}>
+                                                    Low</option>
+                                                <option value="medium"
+                                                    {{ $task->priority == 'medium' ? 'selected' : '' }}>Medium</option>
+                                                <option value="high"
+                                                    {{ $task->priority == 'high' ? 'selected' : '' }}>High</option>
+                                            </select>
+                                        </div>
                                         <button type="submit"
-                                            class="text-sm bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">Update
+                                            class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-black bg-blue-500 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">Update
                                             task</button>
                                     </form>
+
 
                                     <!-- Delete task form -->
                                     <form method="post" action="{{ route('tasks.destroy', $task) }}" class="mt-2">
