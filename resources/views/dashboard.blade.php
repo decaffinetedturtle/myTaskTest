2ec342b254a7caf7ff13ba5c17f8781c583139f6
decaffinetedturtle <syakirahmadakhtar@gmail.com>
2023-06-10 09:47:22 +0800

    Made code more readable

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -39,8 +39,10 @@
                             </select>
                         </div>
                         <button type="submit"
-                            class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Add
-                            task</button>
+                            class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm
+                            font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2
+                            focus:ring-offset-2 focus:ring-indigo-500">
+                            Add task</button>
                     </form>
 
                     <div class="mt-4">
